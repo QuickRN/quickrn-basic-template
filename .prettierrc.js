efa195a82f7ac893b06a2e8b39a7032637f9356b
Henry Paulino <henrypl360@gmail.com>
2021-12-04 12:57:58 +0000

    install default packages and config project

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
   singleQuote: true,
   trailingComma: 'all',
+  semi: false,
   arrowParens: 'avoid',
+  printWidth: 90,
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-};
+  presets: [
+    [
+      'module:metro-react-native-babel-preset',
+      { useTransformReactJSXExperimental: true },
+    ],
+  ],
+  plugins: [
+    [
+      '@babel/plugin-transform-react-jsx',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        cwd: 'babelrc',
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        '@lib': './src/lib',
+        '@hooks': './src/hooks',
+        '@assets': './src/assets',
+        '@screens': './src/screens',
+        '@components': './src/components',
+        '@navigation': './src/navigation',
+      },
+    ],
+  ],
+}
